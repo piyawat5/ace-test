2408cc502f9198bc8b40dcac4046dadfd3f58178
piyawat pintusornsri <piyawat@softever.co.th>
2024-02-03 15:41:55 +0700

    fix logic get api

--- src/components/LaunchesTableComponent.vue
+++ src/components/LaunchesTableComponent.vue
@@ -19,15 +19,26 @@
       v-model:search="search"
       class="table"
       sortable
-      :items="items"
+      :items="rows"
     >
       <template v-slot:[`item.image`]="{ item }">
         <v-card class="my-2" elevation="2" rounded>
-          <v-img :src="`${item.image}`" height="64" cover></v-img>
+          <v-img
+            v-if="item.image"
+            :src="`${item.image}`"
+            height="64"
+            cover
+          ></v-img>
+          <img
+            v-else
+            alt="logo"
+            :src="require('@/assets/notfound.png')"
+            height="64"
+          />
         </v-card>
       </template>
       <template v-slot:[`item.id`]="{ item }">
-        <div class="text-link">{{ item.id }}</div>
+        <div @click="toggleModal(item.id)" class="text-link">{{ item.id }}</div>
       </template>
       <template v-if="statusProp === 'ALL'" v-slot:[`item.status`]="{ item }">
         <status-component :statusProp="item.status"></status-component>
@@ -39,6 +50,27 @@
         <div>{{ item.crew.length }}</div>
       </template>
     </v-data-table>
+    <v-dialog v-model="isOpenModal" width="auto">
+      <v-card>
+        <v-card-text class="modal-head"> รายละเอียด </v-card-text>
+        <v-card-text>
+          <div>รายละเอียด: {{ row?.details }}</div>
+          <div>ข้อมูลจรวด: {{ row?.rocket }}</div>
+          <div>
+            นักบิน:
+            <div v-for="item in row?.crew" :key="item.id">
+              {{ item.crew }}
+            </div>
+          </div>
+          <div>ฐานที่ยิง: {{ row?.launchpad }}</div>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" block @click="isOpenModal = false"
+            >Close Dialog</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-card>
 </template>
 
@@ -49,6 +81,7 @@ import axios from "axios";
 import { onMounted } from "vue";
 import { Column, statusEnum } from "@/components/type";
 import { useDate } from "vuetify";
+
 export default {
   name: "HomeView",
   props: {
@@ -61,68 +94,81 @@ export default {
     StatusComponent,
   },
   setup(props: any) {
-    const API_URL = "https://api.spacexdata.com/latest";
-
-    const getLaunches = () => {
-      return axios.get(`${API_URL}/launches`);
-    };
-    const items = ref<Column[]>([]);
+    const rows = ref<Column[]>([]);
+    const row = ref();
     const search = ref("");
     const date = useDate();
+    const isOpenModal = ref(false);
     const loading = ref(false);
+    const API_URL = "https://api.spacexdata.com/v5";
 
-    const checkStatus = (value: Date) => {
-      const launchDate = new Date(value).getTime();
-      if (launchDate <= Date.now()) {
-        return statusEnum.LAUNCHED;
+    const getLaunches = () => {
+      if (props.status === statusEnum.ALL) {
+        return axios.get(`${API_URL}/launches`);
+      } else if (props.status === statusEnum.LAUNCHED) {
+        return axios.get(`${API_URL}/launches/past`);
       }
-      return statusEnum.UNLAUNCH;
+      return axios.get(`${API_URL}/launches/upcoming`);
+    };
+
+    const getLaunchesById = (id: string) => {
+      return axios.get(`${API_URL}/launches/${id}`);
+    };
+
+    const toggleModal = async (id: string) => {
+      try {
+        isOpenModal.value = true;
+        const lauchesById = await getLaunchesById(id);
+        row.value = { ...lauchesById.data };
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const checkStatus = (isUpcoming: boolean) => {
+      if (isUpcoming) {
+        return statusEnum.UNLAUNCH;
+      }
+      return statusEnum.LAUNCHED;
     };
 
     onMounted(async () => {
       loading.value = true;
-      const now = Date.now();
-
       try {
         const res = await getLaunches();
-
-        items.value = res.data
-          .filter((item: any) => {
-            if (props.status === statusEnum.ALL) {
-              return true;
-            } else if (props.status === statusEnum.LAUNCHED) {
-              const launchDate = new Date(item.date_local).getTime();
-              return launchDate <= now;
-            } else {
-              const launchDate = new Date(item.date_local).getTime();
-              return launchDate > now;
-            }
-          })
-          .map((item: any) =>
-            props.status === statusEnum.ALL
-              ? {
-                  name: item.name,
-                  image: item.links.patch.small,
-                  id: item.id,
-                  status: checkStatus(item.date_local),
-                  date: date.format(item.date_local, "fullDateWithWeekday"),
-                  crew: [...item.crew],
-                }
-              : {
-                  name: item.name,
-                  image: item.links.patch.small,
-                  id: item.id,
-                  date: date.format(item.date_local, "fullDateWithWeekday"),
-                  crew: [...item.crew],
-                }
-          );
+        rows.value = res.data.map((item: any) =>
+          props.status === statusEnum.ALL
+            ? {
+                name: item.name,
+                image: item.links.patch?.small,
+                id: item.id,
+                status: checkStatus(item.upcoming),
+                date: date.format(item.date_local, "fullDateWithWeekday"),
+                crew: [...item.crew],
+              }
+            : {
+                name: item.name,
+                image: item.links.patch?.small,
+                id: item.id,
+                date: date.format(item.date_local, "fullDateWithWeekday"),
+                crew: [...item.crew],
+              }
+        );
         loading.value = false;
       } catch (error) {
         console.log(error);
       }
     });
 
-    return { items, search, loading, statusProp: props.status };
+    return {
+      rows,
+      row,
+      search,
+      loading,
+      statusProp: props.status,
+      isOpenModal,
+      toggleModal,
+    };
   },
 };
 </script>
@@ -135,5 +181,13 @@ export default {
   cursor: pointer;
   text-decoration: underline;
   color: #002992;
+}
+
+.modal-head {
+  text-align: center;
+  font-weight: bold;
+  font-size: large;
+  color: white;
+  background-color: #002992;
 }
 </style>
